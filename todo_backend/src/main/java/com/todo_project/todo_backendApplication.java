754292255737042594d0b6f8c96e7cfc59c8f93c
ritesh-k-g <ritesh.kumargupta.7549@gmail.com>
2024-12-27 22:39:39 +0530

    Make a simple api

--- todo_backend/src/main/java/com/todo_project/todo_backendApplication.java
+++ todo_backend/src/main/java/com/todo_project/todo_backendApplication.java
@@ -2,30 +2,36 @@ package com.todo_project;
 
 import com.todo_project.api.Route;
 import io.dropwizard.Application;
-import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.hibernate.SessionFactory;
 
 public class todo_backendApplication extends Application<todo_backendConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         new todo_backendApplication().run(args);
     }
 
     @Override
-    public String getName() {
-        return "todo_backend";
-    }
+    public void run(todo_backendConfiguration configuration, Environment environment) {
+        String url = configuration.getDatabaseUrl();
+        String user = configuration.getDatabaseUser();
+        String password = configuration.getDatabasePassword();
 
-    @Override
-    public void initialize(final Bootstrap<todo_backendConfiguration> bootstrap) {
-        // TODO: application initialization
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Successfully connected to the database!");
+        } catch (Exception e) {
+            System.err.println("Failed to connect to the database:");
+            e.printStackTrace();
+        }
+        Route route = new Route();
+        environment.jersey().register(route);
+//        final SessionFactory sessionFactory = new HibernateBundle<>(TestEntity.class).getSessionFactory();
+//        final TestEntityDAO testEntityDAO = new TestEntityDAO(sessionFactory);
+//
+//        environment.jersey().register(new TestEntityResource(testEntityDAO));
     }
-
-    @Override
-    public void run(final todo_backendConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-        Route route = new Route();environment.jersey().register(route);
-    }
-
 }
---
+++ todo_backend/src/main/java/com/todo_project/utils/Status.java
@@ -0,0 +1,8 @@
+package com.todo_project.utils;
+
+public enum Status {
+    CREATED,
+    WIP,
+    DONE,
+    DELETED
+}
